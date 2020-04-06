b2f4dc6bd44342a3493f532fd126c3de1192b543
TonyBHerrera <tonybherrera@yahoo.com>
2020-04-06 14:57:28 -0600

    Deep Dive: Props, State, and this - Part 2

--- src/components/app.js
+++ src/components/app.js
@@ -7,7 +7,6 @@ export default class App extends Component {
       <div className='app'>
         <h1>React, Props, and State Deep Dive</h1>
         <JournalList heading="List 1" />
-        <JournalList heading="List 2" />
         <h2>React Skeleton</h2>
       </div>
     );
--- src/components/journals/journal_list.js
+++ src/components/journals/journal_list.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import { JournalEntry } from './journalEntry'
 
 const rawJournalData = [
     { title: 'Post One', content: 'Post content', status: "draft" },
     { title: 'Post Two', content: 'Post content', status: "Published" },
     { title: 'Post Three', content: 'Post content', status: "Published" },
     { title: 'Post Four', content: 'Post content', status: "Published" }
-
 ]
+
+
 // Class component 
 export default class JournalList extends Component {
     constructor(props) {
@@ -18,7 +20,44 @@ export default class JournalList extends Component {
             isOpen: true
         }
     }
+
+    clearEntries = () => {
+        this.setState({ journalData: [], isOpen: false })
+    }
+
+    showAllEntries = () => {
+        this.setState({ journalData: rawJournalData, isOpen: true })
+    }
+
+    toggleStatus = () => {
+        if (this.state.isOpen) {
+            this.setState({ journalData: [], isOpen: false })
+        } else {
+            this.setState({ journalData: rawJournalData, isOpen: true })
+        }
+    }
+
     render() {
-        return <h2>{this.props.heading}</h2>
+        const journalEntries = this.state.journalData.map(journalEntry => {
+            return (
+                <div key={journalEntry.title}>
+                    <JournalEntry
+                        title={journalEntry.title}
+                        content={journalEntry.content}
+                    />
+                </div>
+            )
+        })
+
+        return (
+            <div>
+                <h2>{this.props.heading}</h2>
+                {journalEntries}
+
+                <button onClick={this.clearEntries}>Clear Journal Entries</button>
+                <button onClick={this.showAllEntries}>Show All Journal Entries</button>
+                <button onClick={this.toggleStatus}>Toggle Entries</button>
+            </div>
+        )
     }
 }
